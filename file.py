f8e0d146b0ac304228e8c1003233b2774c6150c9
jamalosm <nainajamal03@gmail.com>
2025-06-28 16:00:28 +0530

    new

---
+++ file.py
@@ -0,0 +1,20 @@
+try:
+    a=int(input())
+    b=int(input())
+    
+
+except Exception as e:
+    print(e)
+
+
+else:
+    print(a/b)
+
+
+finally:
+    print('program over ')
+ 
+
+
+
+
---
+++ miii.py
@@ -0,0 +1,10 @@
+import math as e
+
+print(e.sqrt(4))
+
+print(e.factorial(5))
+
+print(e.floor(5.5))
+
+print(e.pow(2,3))
+
